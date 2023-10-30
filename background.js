ddaf28ebace9ced72a2d9eca33c3b382f2653ca3
kalyani <kalyanikulkarni2002@gmail.com>
2023-10-30 17:39:56 +0530

    Starting from the start

--- background.js
+++
@@ -1,7 +0,0 @@
-console.log("Background script is running.");
-
-chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-    chrome.runtime.sendNativeMessage('com.example.native', message, (response) => {
-        console.log('Received response from native host:', response);
-    });
-});
