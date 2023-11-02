418cf64d0c139d9189688f14ae5066e1b3a9c887
kalyani <kalyanikulkarni2002@gmail.com>
2023-11-02 14:26:23 +0530

    final final revision

---
+++ accept.py
@@ -0,0 +1,19 @@
+from flask import Flask, request, render_template
+# Import model hereee
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/check_ssrfs', methods=['POST'])
+def check_ssrfs():
+    url = request.form.get('url')
+    # get result here
+    result = False
+
+    return render_template('result.html', result=result, url=url)
+
+if __name__ == '__main__':
+    app.run(debug=True)
