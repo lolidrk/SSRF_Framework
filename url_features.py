588f3895cced1d209eb2eab737e1a19b8238ee25
kalyani <kalyanikulkarni2002@gmail.com>
2023-11-04 15:43:53 +0530

    initial version

--- url_features.py
+++ url_features.py
@@ -1,7 +1,7 @@
 import urllib.request
 from bs4 import BeautifulSoup
 import re
-
+'''
 def extract_url_feat(url):
     try:
         #here i'll put the code to get the features
@@ -9,4 +9,11 @@ def extract_url_feat(url):
     except Exception as e:
         print("Error: ", e)
         return None
-    
+  '''
+url =   "https://github.com/lolidrk/Breaking_monoliths/blob/main/microservices/23/addition/app.py"
+
+domain_tokens = url.split('/')
+domain_token_count = len(domain_tokens)
+
+avgpathtokenlen = len(url) / domain_token_count
+
