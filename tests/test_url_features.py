b03b80c16243f6306e04a21f935a6f2275bed894
kalyani <kalyanikulkarni2002@gmail.com>
2023-11-06 10:47:08 +0530

    Revert " this commit is gonna be reverted"
    This reverts commit ae6b2c6825437521541d7dd50da3e238ab250cfc.

--- tests/test_url_features.py
+++ tests/test_url_features.py
@@ -1,8 +1,7 @@
-import url_features
+from url_features import calculate_spchar_url
 import pytest
 
 @pytest.mark.parametrize("test_input,expected", [("www.google.com", 0)])
 def test_calculate_spchar_url(test_input, expected):
-    result = url_features.calculate_spchar_url(test_input)
-    assert result == expected
+    assert calculate_spchar_url(test_input) == expected
 
