ae6b2c6825437521541d7dd50da3e238ab250cfc
kalyani <kalyanikulkarni2002@gmail.com>
2023-11-06 10:46:18 +0530

    this commit is gonna be reverted

--- tests/test_url_features.py
+++ tests/test_url_features.py
@@ -1,7 +1,8 @@
-from url_features import calculate_spchar_url
+import url_features
 import pytest
 
 @pytest.mark.parametrize("test_input,expected", [("www.google.com", 0)])
 def test_calculate_spchar_url(test_input, expected):
-    assert calculate_spchar_url(test_input) == expected
+    result = url_features.calculate_spchar_url(test_input)
+    assert result == expected
 
